ba0c90d3584ff4c84be2b7d68b6b1c02542a8fcb
dav <137420943+davosuffers@users.noreply.github.com>
2026-04-18 01:50:24 -0700

    final

--- scripts.js
+++ scripts.js
@@ -1,97 +1,111 @@
-/**
- * Data Catalog Project Starter Code - SEA Stage 2
- *
- * This file is where you should be doing most of your work. You should
- * also make changes to the HTML and CSS files, but we want you to prioritize
- * demonstrating your understanding of data structures, and you'll do that
- * with the JavaScript code you write in this file.
- *
- * The comments in this file are only to help you learn how the starter code
- * works. The instructions for the project are in the README. That said, here
- * are the three things you should do first to learn about the starter code:
- * - 1 - Change something small in index.html or style.css, then reload your
- *    browser and make sure you can see that change.
- * - 2 - On your browser, right click anywhere on the page and select
- *    "Inspect" to open the browser developer tools. Then, go to the "console"
- *    tab in the new window that opened up. This console is where you will see
- *    JavaScript errors and logs, which is extremely helpful for debugging.
- *    (These instructions assume you're using Chrome, opening developer tools
- *    may be different on other browsers. We suggest using Chrome.)
- * - 3 - Add another string to the titles array a few lines down. Reload your
- *    browser and observe what happens. You should see a fourth "card" appear
- *    with the string you added to the array, but a broken image.
- *
- */
-
-const FRESH_PRINCE_URL =
-  "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL =
-  "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL =
-  "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
-
-// This is an array of strings (TV show titles)
-let titles = [
-  "Fresh Prince of Bel Air",
-  "Curb Your Enthusiasm",
-  "East Los High",
+const guitars = [
+  {
+    model: "Les Paul Standard",
+    year: 1959,
+    type: "Electric",
+    price: 2999,
+    image: "https://commons.wikimedia.org/wiki/Special:FilePath/Gibson_Les_Paul_Standard_(cherry_sunburst).jpg?width=600",
+  },
+  {
+    model: "Les Paul Custom",
+    year: 1954,
+    type: "Electric",
+    price: 3499,
+    image: "https://commons.wikimedia.org/wiki/Special:FilePath/Gibson_Les_Paul_54_Custom.jpg?width=600",
+  },
+  {
+    model: "SG Standard",
+    year: 2014,
+    type: "Electric",
+    price: 1799,
+    image: "https://commons.wikimedia.org/wiki/Special:FilePath/2014_Gibson_SG_Standard_Walnut_(16342692114).jpg?width=600",
+  },
+  {
+    model: "ES-335",
+    year: 2005,
+    type: "Electric",
+    price: 2899,
+    image: "https://commons.wikimedia.org/wiki/Special:FilePath/2005_Gibson_ES-335_-_body_angled_-_BB_King_Tone_with_some_Photoshop_(2010-03-19_09.09.44_by_John_Tuggle).jpg?width=600",
+  },
+  {
+    model: "Flying V",
+    year: 1958,
+    type: "Electric",
+    price: 1599,
+    image: "https://commons.wikimedia.org/wiki/Special:FilePath/Gibson_FlyingV.jpg?width=600",
+  },
+  {
+    model: "Explorer",
+    year: 1958,
+    type: "Electric",
+    price: 1699,
+    image: "https://commons.wikimedia.org/wiki/Special:FilePath/Gibson_Explorer_dark_red.jpg?width=600",
+  },
+  {
+    model: "Firebird",
+    year: 2009,
+    type: "Electric",
+    price: 1899,
+    image: "https://commons.wikimedia.org/wiki/Special:FilePath/Gibson_Firebird_noBG.png?width=600",
+  },
+  {
+    model: "J-45",
+    year: 1942,
+    type: "Acoustic",
+    price: 2499,
+    image: "https://commons.wikimedia.org/wiki/Special:FilePath/Gibson_J-45_(by_Jim_Hutcheson).jpg?width=600",
+  },
+  {
+    model: "Hummingbird",
+    year: 1960,
+    type: "Acoustic",
+    price: 3999,
+    image: "https://commons.wikimedia.org/wiki/Special:FilePath/Gibson-Hummingbird.jpg?width=600",
+  },
+  {
+    model: "Thunderbird Bass",
+    year: 2013,
+    type: "Bass",
+    price: 1999,
+    image: "https://commons.wikimedia.org/wiki/Special:FilePath/Gibson_Thunderbird_Bass.jpg?width=600",
+  },
 ];
-// Your final submission should have much more data than this, and
-// you should use more than just an array of strings to store it all.
 
-// This function adds cards the page to display the data in the array
-function showCards() {
-  const cardContainer = document.getElementById("card-container");
-  cardContainer.innerHTML = "";
-  const templateCard = document.querySelector(".card");
+function showGuitars(list) {
+  const container = document.getElementById("card-container");
+  container.innerHTML = "";
 
-  for (let i = 0; i < titles.length; i++) {
-    let title = titles[i];
-
-    // This part of the code doesn't scale very well! After you add your
-    // own data, you'll need to do something totally different here.
-    let imageURL = "";
-    if (i == 0) {
-      imageURL = FRESH_PRINCE_URL;
-    } else if (i == 1) {
-      imageURL = CURB_POSTER_URL;
-    } else if (i == 2) {
-      imageURL = EAST_LOS_HIGH_POSTER_URL;
-    }
-
-    const nextCard = templateCard.cloneNode(true); // Copy the template card
-    editCardContent(nextCard, title, imageURL); // Edit title and image
-    cardContainer.appendChild(nextCard); // Add new card to the container
-  }
+  list.forEach((g) => {
+    container.innerHTML +=
+    `
+      <div class="card">
+        <img src="${g.image}" alt="${g.model}">
+        <h2>${g.model}</h2>
+        <p>Year: ${g.year}</p>
+        <p>Type: ${g.type}</p>
+        <p>Price: $${g.price}</p>
+      </div>
+    `;
+  });
 }
 
-function editCardContent(card, newTitle, newImageURL) {
-  card.style.display = "block";
+function applyFilters() {
+  const query = document.getElementById("search").value.toLowerCase();
+  const type = document.getElementById("type-filter").value;
+  const sortBy = document.getElementById("sort").value;
 
-  const cardHeader = card.querySelector("h2");
-  cardHeader.textContent = newTitle;
+  let result = guitars.filter((g) => {
+    const matchesQuery = g.model.toLowerCase().includes(query);
+    const matchesType = type === "All" || g.type === type;
+    return matchesQuery && matchesType;
+  });
 
-  const cardImage = card.querySelector("img");
-  cardImage.src = newImageURL;
-  cardImage.alt = newTitle + " Poster";
+  if (sortBy === "price-asc")  result.sort((a, b) => a.price - b.price);
+  if (sortBy === "price-desc") result.sort((a, b) => b.price - a.price);
+  if (sortBy === "year-asc")   result.sort((a, b) => a.year - b.year);
+  if (sortBy === "year-desc")   result.sort((a, b) => b.year - a.year);
 
-  // You can use console.log to help you debug!
-  // View the output by right clicking on your website,
-  // select "Inspect", then click on the "Console" tab
-  console.log("new card:", newTitle, "- html: ", card);
+  showGuitars(result);
 }
 
-// This calls the addCards() function when the page is first loaded
-document.addEventListener("DOMContentLoaded", showCards);
-
-function quoteAlert() {
-  console.log("Button Clicked!");
-  alert(
-    "I guess I can kiss heaven goodbye, because it got to be a sin to look this good!",
-  );
-}
-
-function removeLastCard() {
-  titles.pop(); // Remove last item in titles array
-  showCards(); // Call showCards again to refresh
-}
+showGuitars(guitars);
